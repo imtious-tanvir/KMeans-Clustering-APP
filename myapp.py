374f23778307f801dae5b1762922e1bbf333e153
Imtious tanvir <82751228+imtious-tanvir@users.noreply.github.com>
2025-09-05 10:07:32 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -30,7 +30,13 @@ else:
 st.subheader("Dataset Preview")
 st.write(df.head())
 
+st.subheader("Data Preprocessing")
+numeric_col = df.select_dtypes(include = np.number).columns.tolist()
+if len(numeric_col) < 2:
+    st.error("Need at least two numeric columns for clustering.")
+    st.stop()
 
+features = st.multiselect("Select feature columns for clustering", numeric_col)
 
 
 
