88bb92a93459922c81761a037384380e62b4a4be
Imtious tanvir <82751228+imtious-tanvir@users.noreply.github.com>
2025-09-05 08:58:21 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.cluster import kMeans
+from sklearn.cluster import KMeans
 
 st.title("KMeans Clustering App")
 
