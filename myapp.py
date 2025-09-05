def7f94945c72c814648c16da793d83af5718dbf
Imtious tanvir <82751228+imtious-tanvir@users.noreply.github.com>
2025-09-05 09:00:29 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -6,4 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
 st.title("KMeans Clustering App")
+st.subheader("Data Science")
 
+st.sidebar.header("Upload CSV file or Use Sample")
+st.sidebar.checkbox("Use Example Data")
