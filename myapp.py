aff7d2d944e67049efc391e6ff8c1c55ab7a74ce
Imtious tanvir <82751228+imtious-tanvir@users.noreply.github.com>
2025-09-07 10:07:38 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -104,7 +104,7 @@ for feature in features:
         valid_input = False
 
 #button
-if button("Predict Cluster"):
+if st.button("Predict Cluster"):
     if valid_input:
         input_df = pd.DataFrame([input_data])[features]
         cluster_pred = model.predict(input_df)
